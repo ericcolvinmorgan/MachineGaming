4c9005d1dc1d0e1d80f4f80ee01eb5f1c092b167
Eric Morgan <accounts@ericcolvinmorgan.com>
2018-09-12 15:35:55 +0200

    BizHawk form setup

---
+++ BizHawkExternalTool/Presentation/CustomMainForm.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace BizHawk.Client.EmuHawk
+{
+    partial class CustomMainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonTest = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonTest
+            // 
+            this.buttonTest.Location = new System.Drawing.Point(256, 133);
+            this.buttonTest.Name = "buttonTest";
+            this.buttonTest.Size = new System.Drawing.Size(320, 176);
+            this.buttonTest.TabIndex = 0;
+            this.buttonTest.Text = "Test";
+            this.buttonTest.UseVisualStyleBackColor = true;
+            this.buttonTest.Click += new System.EventHandler(this.buttonTest_Click);
+            // 
+            // CustomMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonTest);
+            this.Name = "CustomMainForm";
+            this.Text = "CustomMainForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonTest;
+    }
+}
---
+++ BizHawkExternalTool/Presentation/CustomMainForm.cs
@@ -0,0 +1,55 @@
+﻿using BizHawk.Client.ApiHawk;
+using BizHawk.Emulation.Common;
+using System;
+using System.Windows.Forms;
+
+[assembly: BizHawkExternalTool("Testing")]
+namespace BizHawk.Client.EmuHawk
+{
+    public partial class CustomMainForm : Form, IExternalToolForm
+    {
+        [RequiredService]
+        private IMemoryDomains MemoryDomains { get; set; }
+        [RequiredService]
+        private IEmulator Emulator { get; set; }
+        private int CurrentFrame { get; set; }
+        private bool CurrentlyUpdating { get; set; }
+
+        public CustomMainForm()
+        {
+            InitializeComponent();
+        }
+
+        public bool UpdateBefore => throw new NotImplementedException();
+
+        public bool AskSaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FastUpdate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NewUpdate(ToolFormUpdateType type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Restart()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateValues()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void buttonTest_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
